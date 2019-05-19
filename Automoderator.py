11f0ecbe46b9e8363527de04ea83223db31bf794
Bryan Leow <leow.kenhing.bryan@DHS.SG>
2019-05-20 06:58:47 +0800

    Create Automoderator.py

---
+++ Automoderator.py
@@ -0,0 +1,311 @@
+# ================================================================================================================================= #
+# ================================== r / S G E X A M S   A U T O M O D E R A T O R   C O N F I G ================================== #
+# ===================================================== v e r s i o n   5 . 0 ===================================================== #
+#                                                                    •                                                              #  
+# =========================== u p d a t e d   1 6   M A Y   2 0 1 9   b y   R A N D O M Y S T I C K =========================== #
+# ================================================================================================================================= #
+---
+# WELCOME TO R/SGEXAMS PRIVATE MESSAGE #
+    type: submission
+    is_edited: false
+    message: |
+        Thank you for your submission to SGExams, /u/{{author}}! (⌒∇⌒) While you wait for your post to fill, here are some things you can do in the meantime:
+
+
+
+         • **Check out the pinned megathread at the top of r/SGExams!**
+
+            ➥ These are excellent for general comments where a detailed reply isn't needed, and serves as a common place for relevant parties to mingle.
+
+
+
+         • **Search r/SGExams for past threads on the matter!**
+
+            ➥ We've been around for quite a while now, so your question might already have been answered previously.
+
+
+
+         • **Browse the rest of r/SGExams to help out your fellow peers/juniors!**
+
+            ➥ SGExams wouldn't be what it is today without your continued support and our pay-it-forward culture, and we hope you'll stick around to answer questions on other threads!
+
+
+        *Have trouble getting your post onto r/SGExams? Read our troubleshooting guide [here](https://www.reddit.com/r/SGExams/comments/bp7pe3/meta_posts_not_getting_through_read_me_sgexams/).*
+
+
+        Once again, we wish to extend a warm welcome to you, whether you're a new user posting for the very first time or a regular checking in with the community ⊂(・ヮ・⊂). 
+
+
+        We hope you'll continue being a part of our shared r/SGExams story, and wish you a good time! \\(\^■\^)/
+
+    message_subject: "(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧  Welcome to r/SGExams!  ✧ﾟ･: *ヽ(◕ヮ◕ヽ)"
+
+---
+# Whatsapp/Telegram/Discord/WeChat Groups (for TITLES) #
+    title: [
+      "telegram", "tele",
+      "whatsapp", "whatsap",
+      "discord",
+      "wechat",
+]
+    is_edited: false
+    comment: |
+        r/SGExams has its very own **[Discord](https://discord.gg/c5wEHZz)** and **[Telegram](https://t.me/SGExamsChat)** chat groups for you to interact with fellow SGExams peeps!
+
+        Feel free to join other community-created chat groups if you wish, but do stay safe and exercise discretion as *online interactions outside of the official SGExams Subreddit, Discord and Telegram platforms are not monitored by us.* Joining a WhatsApp group will also compromise your phone number.
+
+        > \>> **Read more @ [A Note on Group Chats - SGExams](https://www.reddit.com/r/SGExams/comments/b3tzzz/a_note_on_group_chats_sgexams/)** <<
+
+        Cheers and stay safe!
+
+---
+# Whatsapp/Telegram/Discord/WeChat Groups (for COMMENTS) #
+    body: [
+      "telegram", "tele",
+      "whatsapp", "whatsap",
+      "discord",
+      "wechat",
+]
+    is_edited: false
+    comment: |
+        r/SGExams has its very own **[Discord](https://discord.gg/c5wEHZz)** and **[Telegram](https://t.me/SGExamsChat)** chat groups for you to interact with fellow SGExams peeps!
+
+        Feel free to join other community-created chat groups if you wish, but do stay safe and exercise discretion as *online interactions outside of the official SGExams Subreddit, Discord and Telegram platforms are not monitored by us.* Joining a WhatsApp group will also compromise your phone number.
+
+        > \>> **Read more @ [A Note on Group Chats - SGExams](https://www.reddit.com/r/SGExams/comments/b3tzzz/a_note_on_group_chats_sgexams/)** <<
+
+        Cheers and stay safe!
+
+
+# ================================================================================================================================
+
+---
+# Auto-Flairing of Posts and User #
+
+title: [ 
+    "[O-Levels]","[O-Level]","[O Levels]","[O Level]" 
+]
+set_flair:
+    template_id: 1cf20642-07a7-11e9-8fb7-0e0d65a9aed0
+author: 
+   set_flair:  
+      template_id: 02ec3a0a-07a8-11e9-a7e6-0e41003b1ad6
+   overwrite_flair: false
+
+
+
+---
+title: [
+    "[A-Levels]","[A-Level]","[A Levels]","[A Level]"
+]
+set_flair:
+    template_id: 2d6f9dfe-07a7-11e9-8f3b-0e0ac9761a4a
+author: 
+   set_flair:  
+      template_id: 0b820fd2-07a8-11e9-8c13-0eb607f17e34
+   overwrite_flair: false
+
+
+
+---
+title: [
+    "[University]","[Uni]"
+]
+set_flair:
+    template_id: 4ee86f06-07a7-11e9-90b3-0e8975bb3ebe
+author: 
+   set_flair:  
+      template_id: 15dfaa7a-07a8-11e9-b025-0e8c76bbd30a
+   overwrite_flair: false
+
+
+
+---
+title: [
+    "[O/A-Levels]","[O/A-Level]","[O/A Levels]","[O/A Level]"
+]
+set_flair:
+    template_id: 771d5c5c-07a7-11e9-af19-0e4319cab466
+
+---
+title: [
+    "[Post-Exams]","[Post-Exam]","[Post Exams]","[Post Exam]"
+]
+set_flair:
+    template_id: 8b24697a-07a7-11e9-b973-0ee7cc9723b4
+
+---
+title: [
+    "[META]"
+]
+set_flair:
+    template_id: a05c271a-07a7-11e9-bfda-0e7e029ca2aa
+
+---
+title: [
+    "[Poly]","[Polytechnic]"
+]
+set_flair:
+    template_id: 2411c4ae-0c33-11e9-a449-0ecdab80ffea
+author: 
+   set_flair:  
+      template_id: 1e4f2436-0c5a-11e9-b115-0eb25bd7c020
+   overwrite_flair: false
+
+
+
+---
+title: [
+    "[IB Diploma]","[IB]"
+]
+set_flair:
+    template_id: 48408d6a-0c33-11e9-83be-0e73afc8ce4a
+
+---
+title: [
+    "[Scholarships]","[Scholarship]",
+]
+set_flair:
+    template_id: c8d742c6-0c32-11e9-b66a-0ee4066cbe44
+
+---
+title: [
+    "[Olympiads]","[Olympiad]","[OLYMPIADS]","[OLYMPIAD]",
+]
+set_flair:
+    template_id: 01e89dbc-0c33-11e9-80d5-0eb91cdc0050
+
+---
+title: [
+    "[BMAT]","[SAT]"
+]
+set_flair:
+    template_id: e2bec34e-0c32-11e9-83ed-0e0d65a9aed0
+
+
+# ================================================================================================================================
+
+---
+# Muting Users #
+    author:
+        comment_karma: "< -1"
+    action: remove
+#comment_karma - compare to the author's comment karma (note that comment karma will not go below -100)
+#post_karma - compare to the author's post karma (note that post karma will not go below 0)
+#combined_karma - compare to the author's combined (comment karma + post karma, combination can not be below -100)
+
+
+# ================================================================================================================================
+
+---
+# Title Tag Requirements - Rule #1 Enforcer #
+    ~title: [
+      "[O-Levels]","[O-Level]","[O Levels]","[O Level]",
+      "[A-Levels]","[A-Level]","[A Levels]","[A Level]",
+      "[University]","[Uni]",
+      "[O/A-Levels]","[O/A-Level]","[O/A Levels]","[O/A Level]",
+      "[Post-Exams]","[Post-Exam]","[Post Exams]","[Post Exam]",
+      "[META]",
+
+      "[Poly]","[Polytechnic]",
+      "[IB Diploma]","[IB]",
+
+      "[Scholarships]","[Scholarship]",
+      "[Olympiads]","[Olympiad]","[OLYMPIADS]","[OLYMPIAD]",
+
+      "[BMAT]","[SAT]",
+
+
+]
+    comment: |
+        Your post has been automatically removed because you did not include one of the required title tags.
+
+        Please read the subreddit post submission rules (Rule #1) for more information.
+    action: remove
+
+
+# ================================================================================================================================
+
+---
+# AutoModerator Report Responses (1/2) #
+    reports: 3
+    modmail: The above {{kind}} by /u/{{author}} has received 3 reports. Please investigate.
+
+---
+# AutoModerator Report Responses (1/2) #
+    reports: 5
+    modmail: The above {{kind}} by /u/{{author}} was removed because it received 5 reports. Please investigate and ensure that this action was correct.
+    action: remove
+
+
+# ================================================================================================================================
+
+---
+# Vulgarity Filter (for TITLES) - Rule #2 & #4 Enforcer #
+    title: [
+      "fuck you", fucker, fuckers,
+      kys, "kill yourself", 
+      knn, ccb, cb, jibai, ji bai, chaojibai, "chao jibai", chao ji bai, lanjiao
+]
+    message: |
+        Your post has been automatically flagged for containing inappropriate phrases and will be reviewed manually by a moderator for hurtful or inflammatory content. Please read the subreddit rules (Rule #2) for more information.
+
+        If this is a false flag, you can safely ignore this message. 
+    action: report
+    action_reason: Automated Vulgarity Filter
+
+---
+# Vulgarity Filter (for COMMENTS) - Rule #2 & #4 Enforcer #
+    body: [
+      "fuck you", fucker, fuckers,
+      kys, "kill yourself", 
+      knn, ccb, cb, jibai, ji bai, chaojibai, "chao jibai", chao ji bai, lanjiao
+]
+    comment: |
+        Your post has been automatically flagged for containing inappropriate phrases and will be reviewed manually by a moderator for hurtful or inflammatory content. Please read the subreddit rules (Rule #2) for more information.
+
+        If this is a false flag, you can safely ignore this message. 
+    action: report
+    action_reason: Automated Vulgarity Filter
+
+
+# ================================================================================================================================
+
+---
+# Removes External Links #
+    type: text submission
+    body (regex, full-text): '(\[.*?\]\()?https?://\S+\)?'
+    action: filter
+
+
+# ================================================================================================================================
+
+# LEGACY/DEFUNCT CONFIGS #
+
+---
+# !remove Command Attempt #
+# Removal Reason: Automod removes the !remove comment instead of the post or comment it is on. Reddit limitation. Correct as at 230219. #
+#moderators_exempt: false
+#type: comment
+#body: "!remove"
+#author:
+#  is_moderator: true
+#
+#action: remove
+#
+#comment: |
+# This post has been removed for violating a rule.
+
+---
+# New Account Restrictions === REMOVED EFFECTIVE 10 MARCH 2019. TESTING PHASE FOR THE NEXT THREE MONTHS FROM SAID DATE. #
+#    author:
+#        account_age: < 1 day
+#        combined_karma: < -1
+#        satisfy_any_threshold: true
+#        is_contributor: false
+#    ~author: [Randomystick, jaydxn1] #whitelist, not usually needed (individual approvals will do)
+#    message: |
+#        Your post has been automatically removed because you do not meet the criteria to post here. Your reddit account must be at least 1 day old and have > -1 karma. Otherwise, your submissions will be automatically deleted by the AutoModerator to prevent spamming and to protect the quality of content posted here. You can message the moderators via modmail to have your content approved and/or your account whitelisted if you don't want to wait for your reddit account to reach 1 day old.
+#    action: remove
+
+# ================================================================================================================================
