da45cf36d39c017e051c87ab8b56bddf9d3f845d
Bryan Leow <bryankhleow@gmail.com>
2020-05-07 19:48:50 +0800

    Add files via upload

---
+++ SGExams_Flair_Assigner.py
@@ -0,0 +1,24 @@
+import praw #reddit
+from csv import DictReader #namelist
+import SGExams_Login
+
+#FLAIRS
+plant2020b = 'a0b11648-71a1-11ea-b345-0ebee32aa3e1'
+plant2020special = 'bbc5a4da-7aa7-11ea-a222-0eda625f8381'
+
+reddit = praw.Reddit(client_id = SGExams_Login.client_id,
+                     client_secret = SGExams_Login.client_secret,
+                     username = SGExams_Login.username,
+                     password = SGExams_Login.password,
+                     user_agent = "reddit:flairchanger by rando",
+                     )
+subreddit = reddit.subreddit("SGExams")
+
+with open(r'C:\Users\Bryan Leow\Documents\Visual Studio 2017\python\namelist.csv', 'r') as read_obj: 
+# first r: convert normal string to raw string (if not directory will generate some unicode error); second r: read-only
+# Note: Change the directory and filename accordingly on your own system
+    csv_dict_reader = DictReader(read_obj)
+    for row in csv_dict_reader:
+        username = reddit.redditor(row['Name'])
+        subreddit.flair.set(username, flair_template_id=plant2020special)
+
