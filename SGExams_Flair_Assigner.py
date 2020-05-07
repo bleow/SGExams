9d50f70ba3ed2f9a5fe4b147e6548caa4d23db61
Bryan Leow <leow.kenhing.bryan@DHS.SG>
2020-05-07 19:54:08 +0800

    Update SGExams_Flair_Assigner.py

--- SGExams_Flair_Assigner.py
+++ SGExams_Flair_Assigner.py
@@ -2,10 +2,6 @@ import praw #reddit
 from csv import DictReader #namelist
 import SGExams_Login
 
-#FLAIRS
-plant2020b = 'a0b11648-71a1-11ea-b345-0ebee32aa3e1'
-plant2020special = 'bbc5a4da-7aa7-11ea-a222-0eda625f8381'
-
 reddit = praw.Reddit(client_id = SGExams_Login.client_id,
                      client_secret = SGExams_Login.client_secret,
                      username = SGExams_Login.username,
@@ -13,6 +9,10 @@ reddit = praw.Reddit(client_id = SGExams_Login.client_id,
                      user_agent = "reddit:flairchanger by rando",
                      )
 subreddit = reddit.subreddit("SGExams")
+
+#FLAIRS
+plant2020b = 'a0b11648-71a1-11ea-b345-0ebee32aa3e1'
+plant2020special = 'bbc5a4da-7aa7-11ea-a222-0eda625f8381'
 
 with open(r'C:\Users\Bryan Leow\Documents\Visual Studio 2017\python\namelist.csv', 'r') as read_obj: 
 # first r: convert normal string to raw string (if not directory will generate some unicode error); second r: read-only
