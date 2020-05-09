61d74de4e087c4206d9312c7af203542c1fb6342
Bryan Leow <leow.kenhing.bryan@DHS.SG>
2020-05-09 23:14:13 +0800

    Update SGExams_Flair_Assigner.py

--- SGExams_Flair_Assigner.py
+++ SGExams_Flair_Assigner.py
@@ -2,6 +2,7 @@ import praw #reddit
 from csv import DictReader #namelist
 import SGExams_Login
 
+# Reddit OAuth Details & Init to SGExams
 reddit = praw.Reddit(client_id = SGExams_Login.client_id,
                      client_secret = SGExams_Login.client_secret,
                      username = SGExams_Login.username,
@@ -10,10 +11,11 @@ reddit = praw.Reddit(client_id = SGExams_Login.client_id,
                      )
 subreddit = reddit.subreddit("SGExams")
 
-#FLAIRS
+# FLAIRS
 plant2020b = 'a0b11648-71a1-11ea-b345-0ebee32aa3e1'
 plant2020special = 'bbc5a4da-7aa7-11ea-a222-0eda625f8381'
 
+# Assign flairs based on csv file of usernames
 with open(r'C:\Users\Bryan Leow\Documents\Visual Studio 2017\python\namelist.csv', 'r') as read_obj: 
 # first r: convert normal string to raw string (if not directory will generate some unicode error); second r: read-only
 # Note: Change the directory and filename accordingly on your own system
